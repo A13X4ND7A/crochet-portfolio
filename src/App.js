fbd2d064466b706745453328554c0a1b44a57b8f
Alexandra <alex@faller.us>
2021-05-24 13:36:05 +0100

    setup routing

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
+import Home from './components/Home';
+import About from './components/About';
+import SinglePost from './components/SinglePost';
+import Post from './components/Post';
+import Project from './components/Project';
+import NavBar from './components/Navbar';
 function App() {
 	return (
-		<div>
-			<h1 className='text-6xl font-thin'>Hello World</h1>
-		</div>
+		<BrowserRouter>
+			<NavBar />
+			<Switch>
+				<Route component={Home} path='/' exact />
+				<Route component={About} path='/about' />
+				<Route component={SinglePost} path='/post/:slug' />
+				<Route component={Post} path='/post' />
+				<Route component={Project} path='/project' />
+			</Switch>
+		</BrowserRouter>
 	);
 }
 
