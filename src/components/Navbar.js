b86d0880c86f1beeeaf08e3b04b679926453d6db
Alexandra <alex@faller.us>
2021-05-24 16:40:14 +0100

    added navbar styling

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,18 +1,36 @@
 import React from 'react';
 import {NavLink} from 'react-router-dom';
+import {SocialIcon} from 'react-social-icons';
 
 export default function Navbar() {
 	return (
-		<header>
-			<div>
-				<nav>
-					<NavLink to='/' exact>
+		<header className='bg-terracotta'>
+			<div className='container mx-auto flex justify-between'>
+				<nav className='flex'>
+					<NavLink to='/' exact activeClassName='navActive' className=' mr-4 text-4xl tracking-widest navStyling py-6 font-thin'>
 						AlexFaller
 					</NavLink>
-					<NavLink to='post'>Blog Posts</NavLink>
-					<NavLink to='project'>Projects</NavLink>
-					<NavLink to='about'>About Me!</NavLink>
+					<NavLink to='post' activeClassName='navActive' className='navStyling py-3'>
+						Blog Posts
+					</NavLink>
+					<NavLink to='project' activeClassName='navActive' className='navStyling py-3'>
+						Projects
+					</NavLink>
+					<NavLink to='about' activeClassName='navActive' className='navStyling py-3'>
+						About Me!
+					</NavLink>
 				</nav>
+				<div className='inline-flex py-3 px-3 my-6'>
+					<SocialIcon url='https://github.com/A13X4ND7A' className=' mr-4 ' target='_blank' fgColor='#fff' style={{height: 35, width: 35}} />
+					<SocialIcon
+						url='https://www.linkedin.com/in/alexandra-faller-8595873a/'
+						className=' mr-4 '
+						target='_blank'
+						fgColor='#fff'
+						style={{height: 35, width: 35}}
+					/>
+					<SocialIcon url='https://twitter.com/A13X4ND7A' className=' mr-4 ' target='_blank' fgColor='#fff' style={{height: 35, width: 35}} />
+				</div>
 			</div>
 		</header>
 	);
--- tailwind-custom-config.js
+++ tailwind-custom-config.js
@@ -2,7 +2,19 @@ module.exports = {
 	purge: [],
 	darkMode: false, // or 'media' or 'class'
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				terracotta: {
+					DEFAULT: '#e07a5f',
+					dark: '#ed5d35',
+				},
+				'greenSheen': '#81b29a',
+				'blueGrey': '#3D405B',
+				'deepChampagne': '#f2cc8f',
+				'eggShell': '#f4f1de',
+			},
+			fontFamily: ['Montserrat'],
+		},
 	},
 	variants: {
 		extend: {},
