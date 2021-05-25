2e7ed3618cabb8ea1e34a4235f27ed1a92e0c382
Alexandra <alex@faller.us>
2021-05-25 18:47:53 +0100

    adding functionality for blog posts

--- src/client.js
+++ src/client.js
@@ -1,6 +1,6 @@
 import sanityClient from '@sanity/client';
 
 export default sanityClient({
-	projectID: 'z8knuzzo',
+	projectId: 'z8knuzzo',
 	dataset: 'production',
 });
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,16 @@
 import React from 'react';
-
+import crochetImage from '../assets/images/olliss-7mJj0Boa4jc-unsplash.jpg';
 export default function Home() {
 	return (
 		<main>
-			<img />
-			<section>
-				<h1>Hi! I'm Alexandra</h1>
+			<img
+				src={crochetImage}
+				alt='Photo by <a href="https://unsplash.com/@olliss?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Olliss</a> on <a href="https://unsplash.com/s/photos/crochet?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+  '
+				className='absolute object-cover w-full h-full'
+			/>
+			<section className='relative flex justify-center min-h-screen pt-12 lg:pt-64 px-8'>
+				<h1 className='text-6xl text-eggShell font-thin tracking-widest leading-none uppercase lg:leading-snug'>Hi! I'm Alexandra</h1>
 			</section>
 		</main>
 	);
